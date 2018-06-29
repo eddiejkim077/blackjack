212ca01d089e9b6d3b17fba17345662a5b90364e
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2018-06-29 00:03:02 -0700

    add readme and clean code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Made a change
+# BLACKJACK
+-
+
+### Basics
+A player's main objective is to beat the dealer with the cards given. A player must also not "bust" or go over the allowed amount of 21. If a player does not bust with the given hand they have the opportunity to outscore the dealer. A dealer must also not go over 21. 21 is **BLACKJACK**.
+
+### Card value
+An Ace counts as 1 or 11.
+
+10, Jack, Queen, King counts as 10.
+
+2-9 counts as face value. 
+
+
+###How to play
+1. All players will have to place a bet.
+2. The dealer will give two cards to each person at the table and two cards to themselves. With 1 card faced down, and 1 card faced up.
+3. If the dealer is showing an Ace they will ask for insurance. 
+4. If the dealer is showing a 10 or Ace they will check to see if they have blackjack, and if they do they will reveal their cards immediately.
+5. If the dealer has blackjack, all bets are taken unless another player has blackjack then it would be a "push".
+6. If the dealer does not have blackjack play begins.
+7. A player has the option to stand, hit, double down, split. 
+8. Stand: A player does not receive anymore cards, and ends turn.
+9. Hit: Player receives one more card or more if they choose, but ends turn if they go over 21.
+10. Double: A player doubles his bet, and only gets one card to end turn. 
+11. Split: If the player has a pair they have the option to split their cards into two hands. All the rules above apply to splitted pairs.
+12. After all players have ended their turn, the dealer will uncover their second card. 
+13. If the dealer has 16 or less they will have to draw another card until they reach 17 or over. 
+14. If the dealer goes over 21, all the players remaining for that deal will win their bet back. 
+15. If the dealer does not bust, then any player that has a higher value in cards will win. 
--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 var masterDeck = buildMasterDeck();
 var nameLookup = {
-    'P': 'Player',
-    'D': 'Dealer'
+	'P': 'Player',
+	'D': 'Dealer'
 };
-
 var chip = new Audio('https://freesound.org/data/previews/201/201805_3745836-lq.mp3');
 var hitSound = new Audio('https://freesound.org/data/previews/96/96127_1554918-lq.mp3');
 var loseSound = new Audio('https://freesound.org/data/previews/96/96129_1554918-lq.mp3');
@@ -18,7 +17,6 @@ var pushSound = new Audio('https://freesound.org/data/previews/240/240776_410774
 var bankroll, betAmount;
 var handInProgress, shuffledDeck, blackjack, winner;
 var dealerHand, dealerSum, playerHand, playerSum;
-
 /*----- cached element references -----*/
 var betEl = document.querySelector('#bet-amt span');
 var bankrollEl = document.querySelector('.bankroll span')
@@ -30,210 +28,202 @@ var gameControlsEl = document.querySelector('.game-controls');
 var hitBtnEl = document.querySelector('.hit');
 var standBtnEl = document.querySelector('.stand');
 var messageEl = document.getElementById('message');
-
 /*----- event listeners -----*/
-document.getElementById('inc-btn').addEventListener('click', function () {
-    if (bankroll < 10) return;
-    betAmount += 10;
-    bankroll -= 10;
-    chip.play();
-    render();
+document.getElementById('inc-btn').addEventListener('click', function() {
+	if (bankroll < 10) return;
+	betAmount += 10;
+	bankroll -= 10;
+	chip.play();
+	render();
 });
-
-document.getElementById('dec-btn').addEventListener('click', function () {
-    if (betAmount < 10) return;
-    betAmount -= 10;
-    bankroll += 10;
-    chip.play();
-    render();
+document.getElementById('dec-btn').addEventListener('click', function() {
+	if (betAmount < 10) return;
+	betAmount -= 10;
+	bankroll += 10;
+	chip.play();
+	render();
 });
-
 dealBtnEl.addEventListener('click', handleDeal);
 hitBtnEl.addEventListener('click', handleHit);
 standBtnEl.addEventListener('click', handleStand);
-
 /*----- functions -----*/
-
 function initialize() {
-    handInProgress = false;
-    winner = null;
-    betAmount = 0;
-    bankroll = 1000;
-    render();
+	handInProgress = false;
+	winner = null;
+	betAmount = 0;
+	bankroll = 1000;
+	render();
 };
 
 function handleUpdateScore(diff, disabled) {
-    betAmount += diff;
-    if (betAmount < 0) {
-        betAmount = 0
-    }
-    if (disabled) {
-        document.querySelector('.deal').disabled = false;
-    }
-    render();
+	betAmount += diff;
+	if (betAmount < 0) {
+		betAmount = 0
+	}
+	if (disabled) {
+		document.querySelector('.deal').disabled = false;
+	}
+	render();
 };
 
 function checkBlackJack() {
-    if (playerSum === 21 && dealerSum === 21) {
-        winner = 'T';
-        betAmount = 0;
-        handInProgress = false;
-        pushSound.play();
-    } else if (playerSum === 21) {
-        blackjack = 'P';
-        bankroll += ((betAmount * 1.5) + betAmount);
-        betAmount = 0;
-        handInProgress = false;
-        blackjSound.play();
-        messageEl.className = "blink_me";
-    } else if (dealerSum === 21) {
-        blackjack = 'D';
-        handInProgress = false;
-        betAmount = 0;
-        dblackjSound.play();
-    }
+	if (playerSum === 21 && dealerSum === 21) {
+		winner = 'T';
+		betAmount = 0;
+		handInProgress = false;
+		pushSound.play();
+	} else if (playerSum === 21) {
+		blackjack = 'P';
+		bankroll += ((betAmount * 1.5) + betAmount);
+		betAmount = 0;
+		handInProgress = false;
+		blackjSound.play();
+		messageEl.className = "blink_me";
+	} else if (dealerSum === 21) {
+		blackjack = 'D';
+		handInProgress = false;
+		betAmount = 0;
+		dblackjSound.play();
+	}
 };
 
 function buildMasterDeck() {
-    var deck = [];
-    suits.forEach(function (suit) {
-        ranks.forEach(function (rank) {
-            deck.push({
-                face: `${suit}${rank}`,
-                value: Number(rank) || (rank === 'A' ? 11 : 10)
-            });
-        });
-    });
-    return deck;
+	var deck = [];
+	suits.forEach(function(suit) {
+		ranks.forEach(function(rank) {
+			deck.push({
+				face: `${suit}${rank}`,
+				value: Number(rank) || (rank === 'A' ? 11 : 10)
+			});
+		});
+	});
+	return deck;
 };
 
 function shuffleDeck() {
-    var tempDeck = masterDeck.slice();
-    shuffledDeck = [];
-    while (tempDeck.length) {
-        var rndIdx = Math.floor(Math.random() * tempDeck.length);
-        shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-    }
+	var tempDeck = masterDeck.slice();
+	shuffledDeck = [];
+	while (tempDeck.length) {
+		var rndIdx = Math.floor(Math.random() * tempDeck.length);
+		shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+	}
 };
 
 function handleDeal() {
-    shuffleDeck();
-    handInProgress = true;
-    winner = blackjack = null;
-    playerHand = [];
-    dealerHand = [];
-    deal(playerHand, 2);
-    deal(dealerHand, 2);
-    playerSum = computeHand(playerHand);
-    dealerSum = computeHand(dealerHand);
-    checkBlackJack();
-    render();
+	shuffleDeck();
+	handInProgress = true;
+	winner = blackjack = null;
+	playerHand = [];
+	dealerHand = [];
+	deal(playerHand, 2);
+	deal(dealerHand, 2);
+	playerSum = computeHand(playerHand);
+	dealerSum = computeHand(dealerHand);
+	checkBlackJack();
+	render();
 };
 
 function handleHit() {
-    deal(playerHand, 1);
-    playerSum = computeHand(playerHand);
-    if (playerSum > 21) {
-        winner = 'D';
-        handInProgress = false;
-        betAmount = 0;
-    }
-    render();
-    hitSound.play();
+	deal(playerHand, 1);
+	playerSum = computeHand(playerHand);
+	if (playerSum > 21) {
+		winner = 'D';
+		handInProgress = false;
+		betAmount = 0;
+	}
+	render();
+	hitSound.play();
 };
 
 function handleStand() {
-    handInProgress = false;
-    dealerPlay();
-    playerSum = computeHand(playerHand);
-    dealerSum = computeHand(dealerHand);
-    if (playerSum === dealerSum) {
-        winner = 'T';
-        bankroll += betAmount;
-        pushSound.play();
-    } else if (dealerSum > playerSum && dealerSum < 22) {
-        winner = 'D';
-        loseSound.play();
-    } else {
-        winner = 'P';
-        bankroll += betAmount * 2;
-        winSound.play();
-    }
-    betAmount = 0;
-    render();
+	handInProgress = false;
+	dealerPlay();
+	playerSum = computeHand(playerHand);
+	dealerSum = computeHand(dealerHand);
+	if (playerSum === dealerSum) {
+		winner = 'T';
+		bankroll += betAmount;
+		pushSound.play();
+	} else if (dealerSum > playerSum && dealerSum < 22) {
+		winner = 'D';
+		loseSound.play();
+	} else {
+		winner = 'P';
+		bankroll += betAmount * 2;
+		winSound.play();
+	}
+	betAmount = 0;
+	render();
 };
 
 function dealerPlay() {
-    while (computeHand(dealerHand) < 17) {
-        deal(dealerHand, 1);
-    }
+	while (computeHand(dealerHand) < 17) {
+		deal(dealerHand, 1);
+	}
 };
 
 function deal(hand, numCards) {
-    hand.push(...shuffledDeck.splice(0, numCards));
+	hand.push(...shuffledDeck.splice(0, numCards));
 };
 
 function computeHand(hand) {
-    var sum = 0;
-    var aces = 0;
-    for (var i = 0; i < hand.length; i++) {
-        var card = hand[i];
-        var ace = card.face.split('').includes('A')
-        if (ace) {
-            aces++;
-        }
-        sum += card.value;
-    }
-    while (sum > 21 && aces) {
-        sum -= 10;
-        aces--;
-    }
-    return sum;
+	var sum = 0;
+	var aces = 0;
+	for (var i = 0; i < hand.length; i++) {
+		var card = hand[i];
+		var ace = card.face.split('').includes('A')
+		if (ace) {
+			aces++;
+		}
+		sum += card.value;
+	}
+	while (sum > 21 && aces) {
+		sum -= 10;
+		aces--;
+	}
+	return sum;
 };
 
 function renderHands() {
-    if (!playerHand) {
-        return;
-    }
-    playerCardsEl.innerHTML = '';
-    //building cards as string of HTML
-    var cardsHtml = playerHand.reduce(function (html, card) {
-        return html + `<div class = "card ${card.face}"></div>`;
-    }, '');
-    playerCardsEl.innerHTML = cardsHtml;
-    dealerCardsEl.innerHTML = '';
-    //building the cards as a string of HTML
-    cardsHtml = dealerHand.reduce(function (html, card, idx) {
-        var cardClass = handInProgress && idx === 0 ? 'back-blue' : card.face;
-        return html + `<div class = "card ${cardClass}"></div>`;
-    }, '');
-    dealerCardsEl.innerHTML = cardsHtml;
+	if (!playerHand) {
+		return;
+	}
+	playerCardsEl.innerHTML = '';
+	//building cards as string of HTML
+	var cardsHtml = playerHand.reduce(function(html, card) {
+		return html + `<div class = "card ${card.face}"></div>`;
+	}, '');
+	playerCardsEl.innerHTML = cardsHtml;
+	dealerCardsEl.innerHTML = '';
+	//building the cards as a string of HTML
+	cardsHtml = dealerHand.reduce(function(html, card, idx) {
+		var cardClass = handInProgress && idx === 0 ? 'back-blue' : card.face;
+		return html + `<div class = "card ${cardClass}"></div>`;
+	}, '');
+	dealerCardsEl.innerHTML = cardsHtml;
 };
 
 function render() {
-    betEl.textContent = betAmount;
-    bankrollEl.textContent = bankroll;
-    renderHands();
-    betControlsEl.style.visibility = handInProgress ? 'hidden' : 'visible';
-    gameControlsEl.style.visibility = handInProgress ? 'visible' : 'hidden';
-    betAmount === 0 ? dealBtnEl.setAttribute('disabled', 'disabled') : dealBtnEl.removeAttribute('disabled');
-    if (blackjack) {
-        messageEl.textContent = `${nameLookup[blackjack]} Has Blackjack!`;
-        messageEl.style.color = 'gold';
-    } else if (winner) {
-        if (winner === 'T') {
-            messageEl.textContent = `Push!`;
-            messageEl.style.color = 'white';
-        } else {
-            messageEl.textContent = `${nameLookup[winner]} Wins!`;
-            messageEl.style.color = winner === 'P' ? 'gold' : 'red';
-
-        }
-    } else {
-        messageEl.textContent = '';
-        messageEl.className = '';
-    }
+	betEl.textContent = betAmount;
+	bankrollEl.textContent = bankroll;
+	renderHands();
+	betControlsEl.style.visibility = handInProgress ? 'hidden' : 'visible';
+	gameControlsEl.style.visibility = handInProgress ? 'visible' : 'hidden';
+	betAmount === 0 ? dealBtnEl.setAttribute('disabled', 'disabled') : dealBtnEl.removeAttribute('disabled');
+	if (blackjack) {
+		messageEl.textContent = `${nameLookup[blackjack]} Has Blackjack!`;
+		messageEl.style.color = 'gold';
+	} else if (winner) {
+		if (winner === 'T') {
+			messageEl.textContent = `Push!`;
+			messageEl.style.color = 'white';
+		} else {
+			messageEl.textContent = `${nameLookup[winner]} Wins!`;
+			messageEl.style.color = winner === 'P' ? 'gold' : 'red';
+		}
+	} else {
+		messageEl.textContent = '';
+		messageEl.className = '';
+	}
 };
-
 initialize();
-
